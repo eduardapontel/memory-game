2d3f33dae067b08285f6500f9325b7ea08245803
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2024-02-25 16:04:43 +0000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,110 @@
+const FRONT = 'card-front';
+const BACK = 'card-back';
+const CARD = 'card';
+const ICON = 'icon';
+
+const proxy = new Proxy(game, {
+    set: function (target, prop, value) {
+        target[prop] = value;
+        showMoves();
+        return true;
+    },
+});
+
+startGame();
+
+function startGame() {
+    game.createCardsFromTechs();
+    initializeCards(game.cards);
+}
+
+function initializeCards() {
+    let gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '';
+    for (card of game.cards) {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    }
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if (face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = `./images/${card.icon}.png`;
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = '&lt/&gt';
+    }
+    element.appendChild(cardElementFace);
+}
+
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add('flip');
+        if (game.secondCard) {
+            proxy.moves++;
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    let gameOverLayer = document.getElementById('gameOver');
+                    gameOverLayer.style.display = 'flex';
+                    getRanking();
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+            }
+        }
+    }
+}
+
+function showMoves() {
+    let moves = document.getElementById('moves');
+    moves.innerHTML = game.moves;
+}
+
+function getRanking() {
+    game.positions.add(game.moves);
+
+    let positionsArray = [...game.positions];
+
+    positionsArray.sort((a, b) => {
+        return a - b;
+    });
+
+    for (let i = 1; i < 4; i++) {
+        localStorage.setItem(`place${i}`, positionsArray[i - 1]);
+        let rankingMoves = localStorage.getItem(`place${i}`);
+        rankingMoves != 'undefined' ? (rankingMoves += ' moves') : (rankingMoves = '-');
+        document.getElementById(`place${i}`).innerHTML = `${rankingMoves}`;
+    }
+}
+
+function restart() {
+    proxy.moves = 0;
+    startGame();
+    let gameOverLayer = document.getElementById('gameOver');
+    gameOverLayer.style.display = 'none';
+}
